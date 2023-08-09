29d6922c986f1317f48d75ed680087019e9928a4
Andrew <Andy>
2023-08-09 21:35:21 +0300

    DZ1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трёхзначное число ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+if (chislo > 99 && chislo < 1000)
+{
+    Console.Write("Вторая цифра -> " + (chislo / 10) % 10);
+}
+else
+{
+  Console.Write(chislo + " - не трёхзначное число, введите число от 100 до 999"); 
+}
